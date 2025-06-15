33ff8ee353fa0d04fd0ff427adda51cb5e559e96
abhinavk90100 <abhinavwith@gmail.com>
2025-06-15 09:46:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2609,11 +2609,11 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
             #     if settings["spell_check"]:
             #         return await advantage_spell_chok(client, name, msg, reply_msg, ai_search)
             #     else:
-            #         return await reply_msg.edit_text(f"**⚠️ No File Found  <br>  MSG HERE @BOLOMOTU ")
+            #         return await reply_msg.edit_text(f"**⚠️ No File Found  <br>  REQUEST HERE @BOLOMOTU ")
 
             if not files: # if no files found
                 results = []
-                original_message = f"**⚠️ No File Found  <br>  MSG HERE @BOLOMOTU SEARCHING**\n** Tr.**"
+                original_message = f"**⚠️ No File Found  <br>  REQUEST HERE @BOLOMOTU SEARCHING**\n** Tr.**"
                 
                 try:
                     # First LibGen attempt
@@ -2670,7 +2670,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                             logger.error(f"Secondary LibGen error: {e}")
 
                         # Final fallback if all attempts fail
-                        return await reply_msg.edit_text(f"**⚠️ No File Found   <br>  MSG HERE @BOLOMOTU") 
+                        return await reply_msg.edit_text(f"**⚠️ No File Found   <br>  REQUEST HERE @BOLOMOTU") 
             
             
         else:
@@ -2864,7 +2864,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
         logger.exception(e)
         reqst_gle = mv_rqst.replace(" ", "+")
         button = [[
-            InlineKeyboardButton("MSG HERE", url=f"https://t.me/bolomotu")
+            InlineKeyboardButton("REQUEST HERE", url=f"https://t.me/bolomotu")
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
@@ -2876,7 +2876,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
     if not movies:
         reqst_gle = mv_rqst.replace(" ", "+")
         button = [[
-            InlineKeyboardButton("MSG HERE", url=f"https://t.me/bolomotu")
+            InlineKeyboardButton("REQUEST HERE", url=f"https://t.me/bolomotu")
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
@@ -2902,7 +2902,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
                 break
         reqst_gle = mv_rqst.replace(" ", "+")
         button = [[
-            InlineKeyboardButton("MSG HERE", url=f"https://t.me/bolomotu{reqst_gle}")
+            InlineKeyboardButton("REQUEST HERE", url=f"https://t.me/bolomotu{reqst_gle}")
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
